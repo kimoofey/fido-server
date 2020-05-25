d69e819ee8a6bb3a0823892326b11b0051370016
Andrey Kim <kimoofey@gmail.com>
2020-05-25 22:12:59 +0300

    init

---
+++ example/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Login from './Login'
+import User from './User'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedIn: false,
+            user: null,
+        }
+    }
+
+    login = (user) => {
+        this.setState({
+            loggedIn: true,
+            user,
+        })
+    };
+
+    logout = () => {
+        this.setState({loggedIn: false})
+    };
+
+    render() {
+        return this.state.loggedIn ?;
+    <
+        User;
+        onLogout = {this.logout};
+        user = {this.state.user};
+        />;
+    : <
+        Login;
+        onLogin = {this.login};
+        />
+    }
+}
+
+export default App;
---
+++ example/src/index.js
@@ -0,0 +1,14 @@
+/**
+ * Dependencies
+ * @ignore
+ */
+import ReactDOM from 'react-dom'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+/**
+ * Mount React App
+ * @ignore
+ */
+ReactDOM.render( < App / >, document.getElementById('root');
+)
